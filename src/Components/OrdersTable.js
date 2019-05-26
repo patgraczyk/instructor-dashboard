2930b7a1817cf8756cb09f09b2d56096a7c398df
Pat <graczyk.pat@gmail.com>
2019-05-26 19:35:14 +0100

    json file added and displayed in the table'

--- src/Components/OrdersTable.js
+++ src/Components/OrdersTable.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import OrdersData from '../Data/OrdersData.json'
 
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
-
-export default function Orders() {
+export default function Orders({data}) {
   return (
     <React.Fragment>
       <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Customer Name</TableCell>
+            <TableCell>Meeting Point</TableCell>
+            <TableCell>Activity</TableCell>
+            <TableCell>Number of people</TableCell>            
+            <TableCell>Total Sale</TableCell>
+            <TableCell align="right">Comission%</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
+          {OrdersData.map(row => (
             <TableRow key={row.id}>
               <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell>{row.time}</TableCell>
+              <TableCell>{row.customerName}</TableCell>
+              <TableCell>{row.activity}</TableCell>
+              <TableCell>{row.numberPeople}</TableCell>
+              <TableCell>{row.totalSale}</TableCell>
+              <TableCell align="right">{row.comission}</TableCell>
             </TableRow>
           ))}
         </TableBody>
